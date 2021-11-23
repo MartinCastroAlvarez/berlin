cd9c2dd0d231a65072ea1d7ee6a5ef62da97ed94
Martin Castro <martin.castro>
2021-11-24 00:06:54 +0100

    feat(node): 👷 Updating Node configuration - Quick fix #devops #node
    modified: App.js

--- App.js
+++ App.js
@@ -2,14 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import { Button } from 'react-native-paper';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button icon="camera">
+          Matar a Nisman
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
